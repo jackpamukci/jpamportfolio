ce8b9fcd1b08bf655cbfc6f252723f338c90e1e3
Jack Pamukci <jackpamukci824@gmail.com>
2023-04-20 21:23:29 -0400

    first commit

---
+++ Documents/code/react-wordpress/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Front from './components/Front';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Blog from './pages/Blog';
+import Projects from './pages/Projects';
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/resume">
+          <Resume/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/blog">
+          <Blog/>
+        </Route>
+        <Route path="/projects">
+          <Projects/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
